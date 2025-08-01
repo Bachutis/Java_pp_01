68c8ee0b8d4db572e2f7208f524492172083d48e
Artūrs Petrivs <arturs131@inbox.lv>
2025-08-01 17:47:48 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        //komentars single line
+        /* multi line
+        */
+
+        String name = "Artūrs";
+
+        char move_left = 'a';
+
+        int age = 33;
+
+    }
+}
+
